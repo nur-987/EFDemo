da7114f806c31f63c3d3a0584d4468e8c53d702a
nur-987 <76473637+nur-987@users.noreply.github.com>
2021-11-15 15:15:06 +0800

    Code based Migration and Automated Migration

---
+++ EFDemo3/DbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDemo3
+{
+    class SchoolMan2DbContext:DbContext
+    {
+        public SchoolMan2DbContext() : base()
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<School> Schools { get; set; }
+    }
+}
---
+++ EFDemo3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDemo3
+{
+    /// <summary>
+    /// Migration Example
+    /// </summary>
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int StudentRollNumber { get; set; }
+        public string StudentRemarks { get; set; }
+        public List<School> SchoolList { get; set; }
+    }
+
+    public class School
+    {
+        public int SchoolId { get; set; }
+        public string Name { get; set; }
+        public Student StudentEnrolled { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
